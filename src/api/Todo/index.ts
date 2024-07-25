5e1514f245373148e242a363921967fd9f859f3d
sohee-an <soan32@hanmail.net>
2024-07-25 19:26:22 +0900

    fix:todoapi로직 수정

--- src/api/Todo/index.ts
+++ src/api/Todo/index.ts
@@ -4,29 +4,33 @@ import { doc, getDoc, setDoc, updateDoc, arrayUnion } from 'firebase/firestore';
 import { TCategory, TTodo } from '../../types/TodoTypes';
 
 class FirebaseActions {
-  static async getDocument(userId: string) {
+  private static async getDocRefAndData(userId: string) {
     const docRef = doc(db, 'todos', userId);
     const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-      return docSnap.data();
-    } else {
-      return null;
-    }
+    return { docRef, data: docSnap.exists() ? docSnap.data() : null };
+  }
+
+  private static getCategoryIndex(data: any, categoryId: string) {
+    return data.todos.findIndex((cat: TCategory) => cat.cid === categoryId);
+  }
+
+  static async getDocument(userId: string) {
+    const { data } = await this.getDocRefAndData(userId);
+    return data;
   }
 
   static async updateDocument(userId: string, data: any) {
-    const docRef = doc(db, 'todos', userId);
+    const { docRef } = await this.getDocRefAndData(userId);
     await updateDoc(docRef, data);
   }
 
   static async setDocument(userId: string, data: any) {
-    const docRef = doc(db, 'todos', userId);
+    const { docRef } = await this.getDocRefAndData(userId);
     await setDoc(docRef, data);
   }
 
   static async addCategory(userId: string, newCategory: TCategory) {
-    const data = await this.getDocument(userId);
-    const docRef = doc(db, 'todos', userId);
+    const { docRef, data } = await this.getDocRefAndData(userId);
     if (data) {
       await updateDoc(docRef, {
         todos: arrayUnion(newCategory),
@@ -39,57 +43,34 @@ class FirebaseActions {
   }
 
   static async updateTodo(userId: string, categoryId: string, newTodo: TTodo) {
-    const data = await this.getDocument(userId);
-    const docRef = doc(db, 'todos', userId);
+    const { docRef, data } = await this.getDocRefAndData(userId);
 
     if (data) {
-      const categoryIndex = data.todos.findIndex(
-        (cat: TCategory) => cat.cid === categoryId
-      );
+      const categoryIndex = this.getCategoryIndex(data, categoryId);
 
       if (categoryIndex > -1) {
-        const updatedTodos = [...data.todos];
-        const category = updatedTodos[categoryIndex];
-        if (!category.item) {
-          category.item = [];
-        }
-        category.item.push(newTodo);
+        data.todos[categoryIndex].item = data.todos[categoryIndex].item || [];
+        data.todos[categoryIndex].item.push(newTodo);
 
-        await updateDoc(docRef, {
-          todos: updatedTodos,
-        });
+        await updateDoc(docRef, { todos: data.todos });
       } else {
         await updateDoc(docRef, {
-          todos: arrayUnion({
-            ...data,
-            item: [newTodo],
-          }),
+          todos: arrayUnion({ ...data, item: [newTodo] }),
         });
       }
     } else {
-      await setDoc(docRef, {
-        todos: [
-          {
-            ...data,
-            item: [newTodo],
-          },
-        ],
-      });
+      await setDoc(docRef, { todos: [{ ...data, item: [newTodo] }] });
     }
   }
 
   static async deleteTodo(userId: string, categoryId: string, todoId: string) {
-    const data = await this.getDocument(userId);
-    const docRef = doc(db, 'todos', userId);
+    const { docRef, data } = await this.getDocRefAndData(userId);
 
     if (data) {
-      const categoryIndex = data.todos.findIndex(
-        (cat: TCategory) => cat.cid === categoryId
-      );
+      const categoryIndex = this.getCategoryIndex(data, categoryId);
 
       if (categoryIndex > -1) {
-        const updatedTodos = [...data.todos];
-        const category = updatedTodos[categoryIndex];
+        const category = data.todos[categoryIndex];
         const todoIndex = category.item.findIndex(
           (todo: TTodo) => todo.id === todoId
         );
@@ -97,9 +78,7 @@ class FirebaseActions {
         if (todoIndex > -1) {
           category.item.splice(todoIndex, 1);
 
-          await updateDoc(docRef, {
-            todos: updatedTodos,
-          });
+          await updateDoc(docRef, { todos: data.todos });
         }
       }
     }
